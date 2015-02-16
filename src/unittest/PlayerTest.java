bde51990df9a823174852811560a5feeb3c45eca
David Karwowski <david.karwowski@uconn.edu>
2015-02-16 15:07:26 -0500

    added imports

--- src/unittest/PlayerTest.java
+++ src/unittest/PlayerTest.java
@@ -1,6 +1,11 @@
 package unittest;
 
 import junit.framework.TestCase;
+import risky.Player;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
 
 public class PlayerTest extends TestCase {
     public PlayerTest(String testName) {
