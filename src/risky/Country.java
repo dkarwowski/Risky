b1695e7eeb5328e61cdd2e685a7c8234b91ce4b6
actoris <thorzeoun.code@gmail.com>
2015-02-16 16:27:10 -0500

    Changes to the Country class. Work in progress.

--- src/risky/Country.java
+++ src/risky/Country.java
@@ -1,5 +1,62 @@
 package risky;
 
-public class Country {
+import java.util.ArrayList;
 
+
+public class Country {
+	private String name = "";
+	private Player owner = null;
+	private int resources;
+	
+    private ArrayList<Spot> spotsInCountry;
+    
+    public Country(String setName, int initialResources){
+    	this.name = setName;
+    	
+    	// initialize the array list of spots for this country
+    	this.setSpotsInCountry(new ArrayList<Spot>());
+    	
+    	this.setResources(initialResources);
+    	
+    }
+
+//--Getters Start-------------------------------------------------------------
+
+    public String getName(){
+    	return this.name;
+    }
+    
+    public Player getOwner(){
+    	return this.owner;
+    }
+    
+	public ArrayList<Spot> getSpotsInCountry() {
+		return spotsInCountry;
+	}
+	
+	public int getResources() {
+		return resources;
+	}
+//--Getters End---------------------------------------------------------------    
+//--Setters Start-------------------------------------------------------------
+	
+	public void setResources(int resourceCount){
+		this.resources = resourceCount;
+	}
+
+//--Getters End---------------------------------------------------------------  
+//--Game Related Functions Start--------------------------------------------------------------- 
+    
+    public void setOwner(Player setOwner){
+    	this.owner = setOwner;
+    }
+    
+    public void updateResources(int newResourceCount){
+    	this.resources += newResourceCount;
+    }
+
+	public void setSpotsInCountry(ArrayList<Spot> spotsInCountry) {
+		this.spotsInCountry = spotsInCountry;
+	}
+	
 }
