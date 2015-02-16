a15ed735deac3a8925bfc5218074eda22f392c60
David Karwowski <david.karwowski@uconn.edu>
2015-02-16 15:57:38 -0500

    added first test

--- src/risky/Player.java
+++ src/risky/Player.java
@@ -10,25 +10,67 @@ public class Player {
 
     // resources the player can place
     private int availableResources;
+    private int resourcesPerTurn;
     private ArrayList<Spot> spotsOwned;
-
+    private ArrayList<Country> countriesOwned;
+    
+    /**
+     * Initialize a player with just a name and ID, sets the resources to defaults of 10&10
+     * @param setName name of the player
+     * @param setID id of the player for the game
+     */
     public Player(String setName, int setID) {
+        this(setName, setID, 10, 10);
+    }
+
+    /**
+     * Initialize the player with all of the information necessary
+     * @param setName name of the player
+     * @param setID id of the player for the game
+     * @param startResources resources the game starts with
+     * @param resourcesPerTurn resources automatically gained per turn
+     */
+    public Player(String setName, int setID, int startResources, int resourcesPerTurn) {
         this.name = setName;
         this.id = setID;
 
         // initialize resources to the amount each player should start with
-        this.availableResources = 10;
+        this.availableResources = startResources;
+        this.resourcesPerTurn = resourcesPerTurn;
 
         // spots Owned is initialized to be empty, first turn will add the first
         // spot(s)
         this.spotsOwned = new ArrayList<Spot>();
+        this.countriesOwned = new ArrayList<Country>();
     }
-
+    
+//--Getters Start-------------------------------------------------------------
+    
     public int getID() {
         return this.id;
     }
 
     public String getName() {
         return this.name;
+    }
+    
+    public int getAvailableResources() {
+        return this.availableResources;
+    }
+    
+    public ArrayList<Spot> getSpotsOwned() {
+        return this.spotsOwned;
+    }
+    
+    public ArrayList<Country> getCountriesOwned() {
+        return this.countriesOwned;
+    }
+    
+//--Getters End---------------------------------------------------------------
+//--Game Related Functions----------------------------------------------------
+    
+    // should add default 
+    public void addResources() {
+        this.availableResources += this.resourcesPerTurn;
     }
 }
--- src/unittest/PlayerTest.java
+++ src/unittest/PlayerTest.java
@@ -1,14 +1,38 @@
 package unittest;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
 import risky.Player;
 
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 
-public class PlayerTest extends TestCase {
-    public PlayerTest(String testName) {
-        super(testName);
+public class PlayerTest {
+    Player player;
+
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("PlayerTest::setUp()\n");
+        player = new Player("test 1", 0);
+    }
+    
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("PlayerTest::tearDown()\n");
+        player = null;
+    }
+    
+    @Test
+    public void addResourceTest() {
+        System.out.println("PlayerTest::addResourceTest()");
+        System.out.println("PlayerTest::addResourceTest:  PlayerResources: " 
+                            + player.getAvailableResources());
+        assertEquals(player.getAvailableResources(), 10);
+        
+        player.addResources(); // function that should automatically add resource
+        assertEquals(player.getAvailableResources(), 20); // should gain 10 resources
+
+        System.out.println("PlayerTest::addResourceTest: PlayerResources: "
+                            + player.getAvailableResources());
     }
 }
