e1f3fb773b5c59e48dd969ae27322c19c28f9094
David Karwowski <david.karwowski@uconn.edu>
2015-02-16 14:25:49 -0500

    creating the initial classes

---
+++ src/risky/Player.java
@@ -0,0 +1,10 @@
+package risky;
+
+public class Player {
+	private String name = "no name";
+	private int id;
+	
+	// resources the player can place
+	private int availableResources;
+	private Spot[] spotsOwned;
+}
---
+++ src/risky/Spot.java
@@ -0,0 +1,5 @@
+package risky;
+
+public class Spot {
+
+}
