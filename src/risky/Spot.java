4b497e5e4eb1c6082da6448203ecb92df77ea073
Dan Fasciano <daniel.fasciano@uconn.edu>
2015-02-16 17:56:59 -0500

    Created fields, getters and setters for the Spot class

--- src/risky/Spot.java
+++ src/risky/Spot.java
@@ -1,5 +1,36 @@
 package risky;
 
-public class Spot {
-
+public class Spot 
+{
+	private String _player;
+	private int _resources;
+	private String _country;
+	
+	//Getters
+	public String getPlayer() 
+	{
+		return _player;
+	}
+	public int getResources()
+	{
+		return _resources;
+	}
+	public String getCountry()
+	{
+		return _country;
+	}
+	
+	//Setters
+	public void setPlayer(String player)
+	{
+		_player = player;
+	}
+	public void setResources(int resources)
+	{
+		_resources = resources;
+	}
+	public void setCountry(String country)
+	{
+		_country = country;
+	}
 }
