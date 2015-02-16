e7ecf6408ae5d28e8e726173712490e289383c92
David Karwowski <karwowski.david@gmail.com>
2015-02-16 14:27:31 -0500

    forgot to add the board

--- src/risky/Player.java
+++ src/risky/Player.java
@@ -4,7 +4,11 @@ public class Player {
 	private String name = "no name";
 	private int id;
 	
+	//The player should know the board it belongs to?
+	
 	// resources the player can place
 	private int availableResources;
 	private Spot[] spotsOwned;
+	
+	public Player()
 }
