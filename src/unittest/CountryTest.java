30cc63c6f6c704eeaf081306993fbee6ad67e006
actoris <thorzeoun.code@gmail.com>
2015-02-16 22:19:35 -0500

    Finished Country class tests.

--- src/unittest/CountryTest.java
+++ src/unittest/CountryTest.java
@@ -26,20 +26,56 @@ public class CountryTest {
     @After
     public void tearDown() throws Exception {
         System.out.println("CountryTest::tearDown()\n");
-        country = null;
+        spot1 = new Spot();
+        spot2 = new Spot();
+    }
+    
+    @Test
+    public void countrySpotOwnershipTest() {
+    	spot1.setPlayer(player1);
+    	spot2.setPlayer(player2);
+    	country.setSpotsInCountry(spot1);
+    	country.setSpotsInCountry(spot2);
+        System.out.println("CountryTest::countrySpotOwnershipTest()");
+        
+        System.out.println("CountryTest::countrySpotOwnershipTest:  Ownership: False ");
+        assertEquals(country.doesPlayerOwnAllSpots(), false); // should both be false
+        
+        spot2.setPlayer(player1);
+        System.out.println("CountryTest::countrySpotOwnershipTest:  Ownership: True ");
+        assertEquals(country.doesPlayerOwnAllSpots(), true); // should both be true
     }
     
     @Test
     public void countryClaimTest() {
     	spot1.setPlayer(player1);
-    	spot2.setPlayer(player2);
+    	spot2.setPlayer(player1);
     	country.setSpotsInCountry(spot1);
     	country.setSpotsInCountry(spot2);
         System.out.println("CountryTest::countryClaimTest()");
-        assertEquals(country.doesPlayerOwnAllSpots(), false); // should both be false
-        
-        spot2.setPlayer(player1);
-        assertEquals(country.doesPlayerOwnAllSpots(), true); // should both be true
+
+        country.claimCountry();
+        System.out.println("CountryTest::countryClaimTest:  Country Claimed: True ");
+        assertEquals(country.getOwner(), country.getSpotsInCountry().get(0).getPlayer()); // should be equal
     }
+    
+    @Test
+    public void countryAddSpotsTest() {
+    	spot1.setPlayer(player1);
+    	spot2.setPlayer(player1);
+        System.out.println("CountryTest::countryAddSpotsTest()");
+
+        System.out.println("CountryTest::countryAddSpotsTest:  SpotsAdded: 0 ");
+        assertEquals(country.getSpotsInCountry().size(), 0); // should be 0
+        
+        country.setSpotsInCountry(spot1);
+        System.out.println("CountryTest::countryAddSpotsTest:  SpotsAdded: 1 ");
+        assertEquals(country.getSpotsInCountry().size(), 1); // should be 1
+        
+        country.setSpotsInCountry(spot2);
+        System.out.println("CountryTest::countryAddSpotsTest:  SpotsAdded: 2 ");
+        assertEquals(country.getSpotsInCountry().size(), 2); // should be 2
+    }
+    
     
 }
