14dfe2312ca283683cfcd35fe859db1e3561b906
Zachary Alberts <zachary.alberts@uconn.edu>
2015-02-16 22:02:25 -0500

    adding constructor and getters to Board

--- src/risky/Board.java
+++ src/risky/Board.java
@@ -2,4 +2,24 @@ package risky;
 
 public class Board {
 
+	private String name = "Untitled Board";
+	private int territories = 42; // default number of territories is 42
+	
+	public Board(String setName, int setTerritories){
+		this.name = setName;
+		this.territories = setTerritories;
+	}
+	
+//--Getters Start----------------------------------------------------------
+	
+	public String getName(){
+		return this.name;
+	}
+	public int getTerritories(){
+		return this.territories;
+	}
+	
+//--Getters End------------------------------------------------------------
+
 }
+
