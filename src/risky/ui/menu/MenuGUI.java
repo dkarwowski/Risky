470a7d2b1eba2d1644675c435bf1f4cfb12368f0
actoris <adam.toris@uconn.edu>
2015-04-03 10:59:01 -0400

    Start main menu

---
+++ src/risky/ui/menu/MenuGUI.java
@@ -0,0 +1,21 @@
+package risky.ui.menu;
+
+import risky.Risky;
+
+import javax.swing.*;
+import java.awt.*;
+
+// TODO(adam): Legit everything. Why did you even push this?
+
+public class MenuGUI extends JFrame{
+
+    public MenuGUI(Risky risky) {
+
+    super("Risky Main Menu"); // initialize with name
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setLayout(new BorderLayout());
+    this.setTitle("Risky Main Menu");
+
+    }
+
+}
